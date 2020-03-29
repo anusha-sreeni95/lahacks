37b43b8a7390bfa4eff4f843b229f8188cc14a73
Anusha Sreenivasan <anushasreenivasan@Anushas-MacBook-Pro.local>
2020-03-28 17:12:27 -0700

    Created model and form for dashboard

--- lahacks/dashboard/models.py
+++ lahacks/dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class LocationData(models.Model):
+    email_address = models.EmailField()
+    longitude = models.DecimalField(max_digits=8, decimal_places=3)
+    latitutde = models.DecimalField(max_digits=8, decimal_places=3)
+    timestamp = models.TextField()
--- lahacks/dashboard/views.py
+++ lahacks/dashboard/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
+from .forms import UserForm
+from django.views.generic.edit import FormView
+from .utils import add_location
+from .models import LocationData
 
 
 # Create your views here.
-class DashboardView(TemplateView):
+class DashboardView(FormView):
     template_name = 'dashboard.html'
+    form_class = UserForm
 
     def get(self, request, *args, **kwargs):
-        email_address = request.session['email_address']
-        if email_address != '':
-            context = {
-                'email_address': email_address
-            }
-            return render(request, self.template_name, context=context)
+        form_class = self.get_form_class()
+        context = {
+            'form_class': form_class
+        }
+        location_data = LocationData.objects.filter(email_address=request.session['email_address'])
+        for data in location_data:
+            print(data.latitutde, data.longitude, data.timestamp)
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        if form.is_valid():
+            location = form.cleaned_data['location']
+            add_location(request, location)
+            return HttpResponseRedirect("/dashboard")
         else:
-            return HttpResponseRedirect("/login")
+            return render(request, self.template_name)
